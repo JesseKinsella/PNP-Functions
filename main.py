5ac511747df3d10598381453f9cd915395d7c302
Jesse Kinsella <89218189+JesseKinsella@users.noreply.github.com>
2022-03-08 21:25:29 +0000

    added code

---
+++ main.py
@@ -0,0 +1,23 @@
+#functions are a way to give a name to a chunk of code
+
+#first define the function. note the colon, do something below...
+def bark():
+  print('Woof woof!') #function that prints. this only names it. Or Call the function
+
+bark()
+
+
+def bark2():
+  print('Woof woof!')
+  print("I'm a dog")
+bark2()
+
+for x in range(1000):
+  bark()
+
+#Create a function that says Hello Nick!
+
+def hi_nick():
+  print("Hello, Nick!")
+
+hi_nick()
